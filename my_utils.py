c015ea365596768dab55e732168c922bb91c4f11
Aaron Pickard <34072933+aaronpickard@users.noreply.github.com>
2019-04-18 03:25:51 -0400

    Added new functions, replace csv_viewer.py
    Function to identify illegal tether length (exceeds maximum allowed based on input)
    Function to plot output path
    Function to plot putdown points
    This file has now taken over all the functionality of csv_viewer.py

--- my_utils.py
+++ my_utils.py
@@ -8,11 +8,15 @@ This file supports Project JENGA, a final project in the Spring 2019 semester of
 MEIE 4810 Introduction to Human Spaceflight at Columbia University.
 """
 import csv
+import csv
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
 
 def in_to_mm(input):
     output = input/0.039370
     return output
-
 
 def map_from_arduino(x, in_min, in_max, out_min, out_max):
     """
@@ -43,4 +47,71 @@ def find_max_z_value(file_name):
             elif float(row[2]) > float(max_val):
                 max_val = float(row[2])
                 max_point = [row[0], row[1], row[2]]
-    print("MAX VALUE = " + str(max_val) + " at POINT = " + str(max_point))
+    print("MAX Z VALUE = " + str(max_val) + " at POINT = " + str(max_point))
+
+
+def find_illegal_tether_length(file_name, tether_len):
+    x = []
+    y = []
+    z = []
+    e = []
+    max_len = float(tether_len)
+    max_point = [0,0,0]
+    with open(str(file_name), 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+        for row in plots:
+            if (row[0] == "ul_output") or (row[0] == "[-999"):
+                pass
+            elif float(row[0]) >= max_len:
+                illegal_val = float(row[0])
+                illegal_point = [row[0], row[1], row[2], row[3]]
+                print("ILLEGAL VALUE X = " + str(illegal_val) + " at POINT = " + str(illegal_point))
+            elif float(row[1]) >= max_len:
+                illegal_val = float(row[1])
+                illegal_point = [row[0], row[1], row[2], row[3]]
+                print("ILLEGAL VALUE Y = " + str(illegal_val) + " at POINT = " + str(illegal_point))
+            elif float(row[2]) >= max_len:
+                illegal_val = float(row[2])
+                illegal_point = [row[0], row[1], row[2], row[3]]
+                print("ILLEGAL VALUE Z = " + str(illegal_val) + " at POINT = " + str(illegal_point))
+            elif float(row[3]) >= max_len:
+                illegal_val = float(row[3])
+                illegal_point = [row[0], row[1], row[2], row[3]]
+                print("ILLEGAL VALUE E= " + str(illegal_val) + " at POINT = " + str(illegal_point))
+
+def show_path():
+    x = []
+    y = []
+    z = []
+    with open('output_coordinates.csv','r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+        for row in plots:
+            if row[0] != 'x':
+                x.append(float(row[0]))
+            if row[1] != 'y':
+                y.append(float(row[1]))
+            if row[2] != 'z':
+                z.append(float(row[2]))
+    plt.plot(x,y,z, label='Loaded from file!')
+    zline = z
+    xline = x
+    yline = y
+    ax.plot3D(xline, yline, zline, 'gray')
+    plt.show()
+
+def show_putdown_points():
+    x = []
+    y = []
+    z = []
+    with open('output_points.csv', 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+        for row in plots:
+            if row[0] != 'x':
+                x.append(float(row[0]))
+            if row[1] != 'y':
+                y.append(float(row[1]))
+            if row[2] != 'z':
+                z.append(float(row[2]))
+    ax = plt.axes(projection='3d')
+    ax.scatter(x, y, z, c=z, cmap='viridis', linewidth=0.5)
+    plt.show()
