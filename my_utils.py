1252a2e13ba768b6cefc7e8ed696a63acc899599
Aaron Pickard <34072933+aaronpickard@users.noreply.github.com>
2019-04-23 20:44:04 -0400

    Axis labels in my_utils
    Added axis labels & chart titls
    Messed with illegal tether lengths function (added no error output)
    Made the path showing function a scatter plot instead of a line
    Added second show_path_2() function that's not actually functional because I thought I could do something with it and show the path better, but it turned out I couldnt

--- my_utils.py
+++ my_utils.py
@@ -8,11 +8,14 @@ This file supports Project JENGA, a final project in the Spring 2019 semester of
 MEIE 4810 Introduction to Human Spaceflight at Columbia University.
 """
 import csv
+from mpl_toolkits import mplot3d
 import csv
 import matplotlib.pyplot as plt
+from matplotlib import cm
+from numpy import linspace
+
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-
 
 def in_to_mm(input):
     output = input/0.039370
@@ -57,6 +60,7 @@ def find_illegal_tether_length(file_name, tether_len):
     e = []
     max_len = float(tether_len)
     max_point = [0,0,0]
+    illegal = 0
     with open(str(file_name), 'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
         for row in plots:
@@ -65,19 +69,28 @@ def find_illegal_tether_length(file_name, tether_len):
             elif float(row[0]) >= max_len:
                 illegal_val = float(row[0])
                 illegal_point = [row[0], row[1], row[2], row[3]]
-                print("ILLEGAL VALUE X = " + str(illegal_val) + " at POINT = " + str(illegal_point))
+                print("ILLEGAL TETHER LENGTH X = " + str(illegal_val) + " at POINT = " + str(illegal_point))
+                illegal = 1
             elif float(row[1]) >= max_len:
                 illegal_val = float(row[1])
                 illegal_point = [row[0], row[1], row[2], row[3]]
-                print("ILLEGAL VALUE Y = " + str(illegal_val) + " at POINT = " + str(illegal_point))
+                print("ILLEGAL TETHER LENGTH Y = " + str(illegal_val) + " at POINT = " + str(illegal_point))
+                illegal = 1
             elif float(row[2]) >= max_len:
                 illegal_val = float(row[2])
                 illegal_point = [row[0], row[1], row[2], row[3]]
-                print("ILLEGAL VALUE Z = " + str(illegal_val) + " at POINT = " + str(illegal_point))
+                print("ILLEGAL TETHER LENGTH Z = " + str(illegal_val) + " at POINT = " + str(illegal_point))
+                illegal = 1
             elif float(row[3]) >= max_len:
                 illegal_val = float(row[3])
                 illegal_point = [row[0], row[1], row[2], row[3]]
-                print("ILLEGAL VALUE E= " + str(illegal_val) + " at POINT = " + str(illegal_point))
+                print("ILLEGAL TETHER LENGTH A= " + str(illegal_val) + " at POINT = " + str(illegal_point))
+                illegal = 1
+            else:
+                pass
+        if illegal == 0:
+            print("NO ILLEGAL TETHER LENGTH FOUND")
+            print("ALL TETHER LENGTHS BETWEEN 0 AND " + str(tether_len) + " IN THE INSTRUCTION SET")
 
 def show_path():
     x = []
@@ -93,16 +106,58 @@ def show_path():
             if row[2] != 'z':
                 z.append(float(row[2]))
     plt.plot(x,y,z, label='Loaded from file!')
+    ax.text2D(0.2, 0.95, "End Effector Path for a Notional 12-Brick Wall", transform=ax.transAxes)
+    ax.set_xlabel('X Axis')
+    ax.set_ylabel('Y Axis')
+    ax.set_zlabel('Z Axis')
     zline = z
     xline = x
     yline = y
-    ax.plot3D(xline, yline, zline, 'gray')
+    # ax.plot3D(xline, yline, zline, 'grey')
+    ax.scatter(xline, yline, zline, zdir='z', s=1, c='blue', depthshade=True)
     plt.show()
+
+def show_path_2():
+    x = []
+    y = []
+    z = []
+    start = 0.0
+    stop = 1.0
+    number_of_lines = 1000
+    cm_subsection = linspace(start, stop, number_of_lines)
+
+    colors = [cm.jet(x) for x in cm_subsection]
+
+    plt.ylabel('Brick Number')
+    plt.show()
+    with open('output_coordinates.csv', 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+        for row in plots:
+            if row[0] != 'x':
+                x.append(float(row[0]))
+            if row[1] != 'y':
+                y.append(float(row[1]))
+            if row[2] != 'z':
+                z.append(float(row[2]))
+    for i, color in enumerate(colors):
+        plt.axhline(i, color=color)
+    plt.plot(x, y, z, label='Loaded from file!')
+    ax.set_xlabel('X Axis')
+    ax.set_ylabel('Y Axis')
+    ax.set_zlabel('Z Axis')
+    zline = z
+    xline = x
+    yline = y
+    ax.plot3D(xline, yline, zline, cmap='viridis')
+    plt.show()
+
 
 def show_putdown_points():
     x = []
     y = []
     z = []
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
     with open('output_points.csv', 'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
         for row in plots:
@@ -113,5 +168,9 @@ def show_putdown_points():
             if row[2] != 'z':
                 z.append(float(row[2]))
     ax = plt.axes(projection='3d')
-    ax.scatter(x, y, z, c=z, cmap='viridis', linewidth=0.5)
+    ax.text2D(0.2, 0.95, "Brick Placement Locations for a Notional 12-Brick Wall", transform=ax.transAxes)
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
+    ax.set_zlabel('Z axis')
+    ax.scatter(x, y, z, c=z, cmap='viridis', linewidth=5)
     plt.show()
